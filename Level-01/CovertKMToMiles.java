aa4b8a28264b4a6a3d98eb6c4ad3cd598e4d4ab4
Avinashkr000 <akum97543@gmail.com>
2025-01-11 13:37:09 +0530

    Program of ConvertKMToMiles

---
+++ Level-01/CovertKMToMiles.java
@@ -0,0 +1,10 @@
+public class CovertKMToMiles {
+    public static void main(String[] args) {
+        double Kilometer = 10.8;
+        double miles;
+		// Convert
+        miles = Kilometer * 1.6;
+		// The Result
+        System.out.println("The distance " + Kilometer + "km in mile is  "+ miles);
+    }
+}
