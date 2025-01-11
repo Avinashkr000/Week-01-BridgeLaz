214ef33577d3b15e25588dffd59f6f7a96101da6
Avinashkr000 <akum97543@gmail.com>
2025-01-11 13:34:48 +0530

    Program of AverageMarks

---
+++ Level-01/AverageMarks.java
@@ -0,0 +1,11 @@
+public class AverageMarks {
+    public static void main(String[] args) {
+        int MarksInMaths = 94;
+        int MarksInPhysics = 95;
+        int MarksInChemistry = 96;
+			//Avg of PCM
+        double avgMarks = (MarksInMaths +MarksInPhysics +MarksInChemistry)/3;
+			// The results 
+        System.out.println( "Sam's average mark in PCM is  " + avgMarks);
+    }
+}
