18240f7a7467129f9f60982abf6c1916d1425630
Avinashkr000 <akum97543@gmail.com>
2025-01-11 13:39:02 +0530

    Added

---
+++ Level-01/FindAge.java
@@ -0,0 +1,12 @@
+    public class FindAge {
+        public static void main(String[] args) {
+			// that's current YearOfBirth
+            int CurrentYear = 2024;
+			// Date of birth of that
+            int YearOfBirth = 2000;
+
+            int age = CurrentYear - YearOfBirth;
+			// Here is the Results
+            System.out.println("Harry's age in 2024 is " + age);
+        }
+    }
