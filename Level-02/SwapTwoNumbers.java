d05bc92574d73738967bf394e255ab357a585c4d
Avinashkr000 <akum97543@gmail.com>
2025-01-11 13:53:30 +0530

    Added

---
+++ Level-02/SwapTwoNumbers.java
@@ -0,0 +1,15 @@
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 10;
+
+		// swapped the two numbers 
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+			// print the result
+        System.out.println("The swapped numbers are = " + number1 + "  and  " + number2);
+
+
+    }
+}
