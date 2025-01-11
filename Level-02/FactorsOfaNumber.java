bf430fb5b1e8fe8a8c95113e1491401a9738678c
Avinashkr000 <akum97543@gmail.com>
2025-01-11 15:28:38 +0530

    Added

---
+++ Level-02/FactorsOfaNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FactorsOfaNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // taking the Input number
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Applyig Loop to find and print factors
+        System.out.println("Factors of " + number + " are:");
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
