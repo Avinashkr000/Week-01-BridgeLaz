4f7adabc4b45cfbee680c337638b725f215ccfc5
Avinashkr000 <akum97543@gmail.com>
2025-01-13 11:35:40 +0530

    Added

---
+++ Level-01/HandshakeCalc.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HandshakeCalc {
+
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students : ");
+        int n = sc.nextInt();
+
+        if (n < 2) {
+            System.out.println("Less than 2 students ");
+        } else {
+            int handshakes = calculateHandshakes(n);
+            System.out.println("The maximum number of handshakes among " + n + " students is: " + handshakes);
+        }
+	}
+}
