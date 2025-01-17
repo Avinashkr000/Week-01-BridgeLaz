a08e0c0eb53973b7087552f4d31869c07d4f7210
Avinashkr000 <akum97543@gmail.com>
2025-01-17 21:21:55 +0530

    Added

---
+++ Level-06-Extras/GCDAndLCMCalculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class GCDAndLCMCalculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        
+        int gcd = calculateGCD(num1, num2);
+        int lcm = calculateLCM(num1, num2, gcd);
+        
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        System.out.println("The LCM of " + num1 + " and " + num2 + " is: " + lcm);
+        
+        sc.close();
+    }
+    
+    // Function to calculate GCD using Euclidean algorithm
+    public static int calculateGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    // Function to calculate LCM using the formula: LCM(a, b) = (a * b) / GCD(a, b)
+    public static int calculateLCM(int a, int b, int gcd) {
+        return (a * b) / gcd;
+    }
+}
